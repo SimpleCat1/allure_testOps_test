22fb323ae3b5c354fd1571a12e4fd1e385877e16
N.Zotov <nikolai.zotov1@simbirsoft.com>
2022-10-31 19:56:30 +0400

    created a test based on a case from allure testOps

---
+++ src/test/java/com/simbirsoft/page/BasePageDemoQA.java
@@ -0,0 +1,11 @@
+package com.simbirsoft.page;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BasePageDemoQA {
+    @BeforeAll
+    public static void helperDriver() {
+        Configuration.startMaximized = true;
+    }
+}
