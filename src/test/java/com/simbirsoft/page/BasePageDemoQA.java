4d77f8cada187895441dcbe2aca07b15e65e937d
N.Zotov <nikolai.zotov1@simbirsoft.com>
2022-10-31 22:34:45 +0400

    fixed test

--- src/test/java/com/simbirsoft/page/BasePageDemoQA.java
+++ src/test/java/com/simbirsoft/page/BasePageDemoQA.java
@@ -1,9 +1,12 @@
 package com.simbirsoft.page;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.simbirsoft.config.CredentialsConfig;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static java.lang.String.format;
 
@@ -12,9 +15,17 @@ public class BasePageDemoQA {
             ConfigFactory.create(CredentialsConfig.class);
     @BeforeAll
     public static void helperDriver() {
+        Configuration.startMaximized = true;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.remote = format("https://%s:%s@selenoid.autotests.cloud/wd/hub/",
                 credentials.login(),
                 credentials.password());
-        Configuration.startMaximized = true;
     }
 }
